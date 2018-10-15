9eabf3bae5c6255ba41f0a4af84e24362e1d3e94
Ricardo <ricardomessier@gmail.com>
2018-10-15 09:32:52 -0500

    first commit

---
+++ src/main/java/com/revature/hibernate_challenge/App.java
@@ -0,0 +1,13 @@
+package com.revature.hibernate_challenge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
